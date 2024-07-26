4047ef560e0d4913d7fc2b3864da93f390d93a89
Domingos chiconela <azevedodomingos78@gmail.com>
2024-07-26 12:22:44 +0200

    feat:created database connection created

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express')
+const express = require("express")
 const dotenv = require("dotenv").config();
+const connectDB = require('./connect/database')
+
+connectDB()
+
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 
-    
-
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
 
 
 
